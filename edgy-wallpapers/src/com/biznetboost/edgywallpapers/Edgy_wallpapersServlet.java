0530a0b99dadc5b66e6d30daff4c0322760a1554
biznetboost <vivekprakash@10.0.0.238>
2016-11-30 00:18:03 -0800

    Initial comming for edgy-wallpapers

---
+++ edgy-wallpapers/src/com/biznetboost/edgywallpapers/Edgy_wallpapersServlet.java
@@ -0,0 +1,12 @@
+package com.biznetboost.edgywallpapers;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Edgy_wallpapersServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
